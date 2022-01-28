1a7f9908b341b9de3418cb3e8755fb1f3c53a0fe
alejandrosalesianos <alejandromartin2013@live.com>
2022-01-28 14:25:10 +0100

    añadido bottommenu

--- flutter_themoviedb/lib/main.dart
+++ flutter_themoviedb/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_themoviedb/pages/home.dart';
+import 'package:flutter_themoviedb/pages/home_menu.dart';
 
 void main() {
   runApp(
@@ -7,10 +8,11 @@ void main() {
       title: 'Named Routes Demo',
       // Start the app with the "/" named route. In this case, the app starts
       // on the FirstScreen widget.
-      initialRoute: '/',
+      initialRoute: '/menu',
       routes: {
         '/': (context) => const HomePage(),
         // When navigating to the "/" route, build the FirstScreen widget.
+        '/menu': (context) => const HomeMenu(),
       },
     ),
   );
